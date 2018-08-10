322766a6774a05bde56d0bf36b033f36deb2f9d3
Ryan Casas <jryancasas@gmail.com>
2018-08-10 13:12:12 +0200

    Primer commit

---
+++ Flashear Recovery.bat
@@ -0,0 +1,44 @@
+@echo off
+color 08
+chcp 1252
+title Herramienta para flashear recovery por fastboot
+CLS
+ECHO.
+ECHO.
+echo    =======================================================================================
+echo    .                  Herramienta para flashear Recovery por fastboot                    .
+echo    .                                    Por Ryan Casas                                   .
+echo    =======================================================================================
+ECHO.
+ECHO.
+ECHO.
+ECHO.  Para que todo vaya bien, sigue estos pasos en este orden:
+ECHO.
+ECHO   · Asegúrate de que tienes el bootloader desbloqueado.
+ECHO   · Asegúrate de que tienes los drivers instalados correctamente.
+ECHO   · Pon el archivo del recovery en la misma carpeta que este script.
+ECHO   · Renombra dicho archivo a recovery.img
+ECHO   · Pon el teléfono en modo fastboot (Mantén pulsado VOL- y Power a la vez).
+ECHO   · Conecta el teléfono al PC.
+ECHO   · Espera 5 segundos.
+ECHO   · Pulsa ENTER.
+ECHO.
+ECHO.
+ECHO.
+PAUSE
+ECHO.
+ECHO   Flasheando el recovery...
+ECHO.
+fastboot flash recovery recovery.img
+ECHO.
+CLS
+ECHO.
+ECHO   Recovery flasheado. Ahora mantén pulsado el botón VOL+ en el télefono.
+ECHO   Mientras mantienes pulsado el VOL+, pulsa ENTER. Esto te llevará al Recovery.
+ECHO.
+PAUSE
+ECHO.
+ECHO   Reiniciando...
+ECHO.
+fastboot reboot
+CLS
